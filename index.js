b24500450895a0ba33c32aacb1a7370560766209
James Jensen <jnjensen@live.com>
2020-02-04 03:33:27 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, { [key]: value })
+}
+
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
