617b3bb4a8e04c3b14c5723e78f73a01f864dcaf
James Jensen <jnjensen@live.com>
2020-02-04 03:34:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 
-
+function deleteFromObjectByKey(object, key){
+  
+}
 
 function destructivelyDeleteFromObjectByKey(object, key){
   delete object[key]
